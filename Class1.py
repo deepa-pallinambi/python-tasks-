557c482ccd4fc4b4e5be87e12b3b8ef0ed895878
deepa-pallinambi <158241309+deepa-pallinambi@users.noreply.github.com>
2024-02-10 22:28:10 -0800

    Add files via upload

--- Class1.py
+++ Class1.py
@@ -10,3 +10,12 @@ print("""
 print("""
 "SDK" stands for "Software Development Kit",where as
 "IDE"stands for"Integrated Development Environment""")
+print("python is an\"awesome\"language")
+print("python\n\t2023")
+print("i\'m from entri.\b")
+print("\65")
+print("\x65")
+print("entri","2023",sep="\n")
+print("entri","2023",sep="\b")
+print("entri","2023",sep="*",end="\b\b\b\b")
+
